a412464d65377b8a16fed804af05fc71a4c6a1e4
leotorresval <leotorresval92@gmail.com>
2024-09-23 22:09:36 -0500

    Pruebas unitarias con mockito

--- src/test/java/catalogodigital/rest/ProductRestTest.java
+++ src/test/java/catalogodigital/rest/ProductRestTest.java
@@ -4,17 +4,14 @@ import catalogodigital.model.Product;
 import catalogodigital.service.ProductService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
